f7460b789ec8c30e66150fb6e4b63136db589be0
Mwai Joseph <joesoftmwai@gmail.com>
2022-06-01 15:51:24 +0300

    protected dashboard access if user isn"t authenticated

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if (!user) navigate('/signin')
+  }, [user, navigate]);
+
   return (
     <div>Dashboard</div>
   )
